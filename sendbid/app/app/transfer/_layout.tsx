28ac32494f7f2302694b99c8f3cdba2af7714f03
Franck Tchouta <franck.tchouta@worldwide-international.business>
2026-07-01 11:46:39 +0200

    fix: remove duplicate app/app directory and frontend build fixes

--- sendbid/app/app/transfer/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-import { t, useLocale } from "../../src/i18n";
-import { useThemedColors } from "../../src/themeContext";
-export default function TransferLayout() {
-  useLocale((st) => st.locale);
-  const colors = useThemedColors();
-  return <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.neutrals.background } }} />;
-}
--- sendbid/app/app/wallet/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import { Stack } from "expo-router";
-import { useThemedColors } from "../../src/themeContext";
-export default function WLayout() {
-  const colors = useThemedColors();
-  return <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.neutrals.background } }} />;
-}
